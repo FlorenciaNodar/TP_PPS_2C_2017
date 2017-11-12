34868c536d83a6fd0c3a2601016576cc4004a33a
Florencia Nodar <fnodar@algeiba.com>
2017-11-12 19:03:21 -0300

    se modifico la alerta comun por la de ionic en los push

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import { Platform,NavController, ModalController } from 'ionic-angular';
+import { Platform,NavController, ModalController,AlertController } from 'ionic-angular';
 import { AsistenciaModalPage } from '../asistencia-modal/asistencia-modal';
 import{ Push, PushToken } from '@ionic/cloud-angular';
-
+import firebase from 'firebase';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public platform: Platform,public navCtrl: NavController,public push: Push, public modalCtrl: ModalController) {
+  usuario;
+  constructor(public platform: Platform,public navCtrl: NavController,public push: Push, public modalCtrl: ModalController,public alertCtrl:AlertController) {
+        this.usuario=firebase.auth().currentUser.email;
 
-    if (platform.is('android')) {
+
+    if (platform.is('android') && this.usuario == "profesor@profesor.com" || this.usuario == "administrativo@administrativo.com") {
          this.RegisterNotification();
     this.Notification();
         }
@@ -36,7 +39,13 @@ export class HomePage {
   
   this.push.rx.notification()
   .subscribe((msg) => {
-    alert(msg.title + ': ' + msg.text);
+    
+         let alert = this.alertCtrl.create({
+            title: "Aviso de Importancia",
+            subTitle: "Ingresaste al aula!",
+            buttons: ['OK']
+          });
+          alert.present();
   });
 }
 
