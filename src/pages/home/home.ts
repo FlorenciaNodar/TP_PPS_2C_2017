60937101378b7c9fcb946b07b6c96a226e95d18e
Florencia Nodar <fnodar@algeiba.com>
2017-11-12 15:38:42 -0300

    se agrego en home el push

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
+import { Platform,NavController, ModalController } from 'ionic-angular';
 import { AsistenciaModalPage } from '../asistencia-modal/asistencia-modal';
+import{ Push, PushToken } from '@ionic/cloud-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,13 +9,36 @@ import { AsistenciaModalPage } from '../asistencia-modal/asistencia-modal';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
+  constructor(public platform: Platform,public navCtrl: NavController,public push: Push, public modalCtrl: ModalController) {
 
+    if (platform.is('android')) {
+         this.RegisterNotification();
+    this.Notification();
+        }
   }
 
   presentModal() {
     let modal = this.modalCtrl.create(AsistenciaModalPage);
     modal.present();
   }  
+
+  
+  private RegisterNotification(){
+  
+  this.push.register().then((t: PushToken) => {
+    return this.push.saveToken(t);
+  }).then((t: PushToken) => {
+    console.log('Token saved:', t.token);
+  });
+}
+
+  private Notification(){
+  
+  this.push.rx.notification()
+  .subscribe((msg) => {
+    alert(msg.title + ': ' + msg.text);
+  });
+}
+
 
 }
